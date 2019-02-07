597bb9f742675f369d4622bf025ec1f5251c5001
Joshua Hufford <joshuamh1135@gmail.com>
2019-02-07 09:11:04 -0500

    Fix things

--- src/video_stream.py
+++ src/video_stream.py
@@ -4,12 +4,12 @@ import os
 class VideoStream:
     def __init__(self, bebop, frame_rate='24_FPS', video_resolution='rec1080_stream480'):
         self.bebop = bebop
-        self.frame_rate = frame_rate
-        self.video_resolution = video_resolution
+        self.frame_rate = frame_rate                # This can be '24_FPS', '25_FPS', or '30_FPS'
+        self.video_resolution = video_resolution    # This can be rec1080_stream480', or 'rec720_stream720'
 
     def _start_drone_stream(self):
-        self.bebop.set_video_framerate('24_FPS')
-        self.bebop.set_video_resolutions('rec1080_stream480')
+        self.bebop.set_video_framerate(self.frame_rate)
+        self.bebop.set_video_resolutions(self.video_resolution)
         self.bebop.start_video_stream()
 
     @staticmethod
